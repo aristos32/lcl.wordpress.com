2ed46195c8194b8c0bcafbd950b2806616296eed
Aristos Aresti <aristos.aresti@gmail.com>
2025-02-08 15:59:21 +0200

    Add initial template files and structure for Emulsion theme

--- wp-content/plugins/wp-learn-debugging/wp-learn-debugging.php
+++ wp-content/plugins/wp-learn-debugging/wp-learn-debugging.php
@@ -13,18 +13,19 @@
 register_activation_hook(__FILE__, 'wp_learn_setup_table_learn_debugging');
 function wp_learn_setup_table_learn_debugging()
 {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'form_submissions';
+	global $wpdb;
 
-    $sql = "CREATE TABLE $table_name (
+	$table_name = $wpdb->prefix . 'form_submissions';
+
+	$sql = "CREATE TABLE $table_name (
       id mediumint(9) NOT NULL AUTO_INCREMENT,
       name varchar (100) NOT NULL,
       email varchar (100) NOT NULL,
       PRIMARY KEY  (id)
     )";
 
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($sql);
+	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+	dbDelta($sql);
 }
 
 /**
@@ -33,10 +34,10 @@ function wp_learn_setup_table_learn_debugging()
 register_deactivation_hook(__FILE__, 'wp_learn_delete_table_learn_debugging');
 function wp_learn_delete_table_learn_debugging()
 {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'form_submissions';
-    $sql = "DROP TABLE IF EXISTS $table_name";
-    $wpdb->query($sql);
+	global $wpdb;
+	$table_name = $wpdb->prefix . 'form_submissions';
+	$sql = "DROP TABLE IF EXISTS $table_name";
+	$wpdb->query($sql);
 }
 
 /**
@@ -45,15 +46,15 @@ function wp_learn_delete_table_learn_debugging()
 add_action('rest_api_init', 'wp_learn_register_routes');
 function wp_learn_register_routes()
 {
-    register_rest_route(
-        'wp-learn-form-submissions-api/v1',
-        '/form-submissions/',
-        array(
-            'methods'             => 'GET',
-            'callback'            => 'wp_learn_debugging_get_form_submissions',
-            'permission_callback' => '__return_true'
-        )
-    );
+	register_rest_route(
+		'wp-learn-form-submissions-api/v1',
+		'/form-submissions/',
+		array(
+			'methods'             => 'GET',
+			'callback'            => 'wp_learn_debugging_get_form_submissions',
+			'permission_callback' => '__return_true'
+		)
+	);
 }
 
 /**
@@ -63,13 +64,13 @@ function wp_learn_register_routes()
  */
 function wp_learn_debugging_get_form_submissions()
 {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'form_submissions';
+	global $wpdb;
+	$table_name = $wpdb->prefix . 'form_submissions';
 
-    $results = $wpdb->get_results("SELECT * FROM $table_name");
+	$results = $wpdb->get_results("SELECT * FROM $table_name");
 
-    error_log($wpdb->last_query);
-    //error_log(print_r($wpdb->queries, true)); // all queries run during this request
+	error_log($wpdb->last_query);
+	//error_log(print_r($wpdb->queries, true)); // all queries run during this request
 
-    return $results;
+	return $results;
 }
